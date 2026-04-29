7a8f0d10a87beaf375e208e33eb47c91151fa2a7
thiago-alvinho <thiago_silva.550@hotmail.com>
2026-04-29 08:41:34 -0300

    Conectando a api ao banco postgres utilizando sequelize e populando uma tabela

--- api-node-sequelize/src/app.js
+++ api-node-sequelize/src/app.js
@@ -1,10 +1,23 @@
 const express = require('express');
+const { Sequelize } = require('sequelize');
 
 const app = express();
 
+const sequelize = new Sequelize('banco_treinamento', 'admin', 'senha_secreta', {
+  host: 'localhost',
+  dialect: 'postgres'
+});
+
 app.use(express.json());
 
-app.get('/teste', (req, res) => {
+app.get('/teste', async (req, res) => {
+  
+  try {
+    await sequelize.authenticate();
+    console.log("Conectou ao banco");
+  } catch (error) {
+    console.log("Erro ao tentar conectar ao banco", error);
+  }
   res
     .status(200)
     .send({ mensagem: 'boas-vindas à API' });
